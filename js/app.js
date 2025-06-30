1a180ff51678df4bffe9fd501f4389b55f1912be
Johanna Hofer <s2310456005@fhooe.at>
2025-06-30 18:35:29 +0200

    alle unnötigen alten Dateien von KWMContacts gelöscht und Fehler in Console behoben

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-// gibt es Service Worker für den Browser
+//does a service worker for this browser exist?
 if ("serviceWorker" in navigator){
-    // wenn ja, dann registrieren
+    //if yes, register
     navigator.serviceWorker.register("sw.js")
         .then(registration => {
                 console.log(`Service Worker registered with scope ${registration.scope}`);
